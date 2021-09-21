cd3dff5af462ebc248083c59611e0cb95ff3af6b
Cory Forsythe <cory@forsythenetworks.com>
2021-09-21 11:24:24 -0500

    adds pagination

--- index.js
+++ index.js
@@ -5,26 +5,43 @@ let configservice = {}
 const Regions = fs.readFileSync('./regions.aws.list', 'utf8').toString().split('\n');
 const ResourceTypes = fs.readFileSync('./resources.aws.list', 'utf8').toString().split('\n');
 let total = 0;
+
+const discovery = async function (region, resourceType, nextToken) {
+    AWS.config.update({ region });
+    configservice = new AWS.ConfigService({ region });
+    return await configservice.listDiscoveredResources({ resourceType: resourceType, includeDeletedResources: false, nextToken: (nextToken == "init" ? "" : nextToken) }).promise();
+}
+
 Promise.all(Regions.map(async (region) => {
-    region=region.replace('\r','');
+    region = region.replace('\r', '');
     if (region != "") {
         console.info
         console.info("Discovering Region " + region);
-        AWS.config.update({ region });
-        configservice = new AWS.ConfigService({ region });
 
-       await Promise.all(ResourceTypes.map(async (rt) => {
-            rt=rt.replace('\r','');
+
+        await Promise.all(ResourceTypes.map(async (rt) => {
+            rt = rt.replace('\r', '');
             if (rt != "") {
-                const res = await configservice.listDiscoveredResources({ resourceType:rt, includeDeletedResources: false }).promise();
-                total += res.resourceIdentifiers.length
-                console.info("|  " +region+"   "+ rt + " " + res.resourceIdentifiers.length);
+                let nextToken = "init";
+                let morepages = true;
+                while (morepages) {
+                    const res = await discovery(region, rt, nextToken);
+                    total += res.resourceIdentifiers.length
+                    console.info("|  " + region + "   " + rt + " " + res.resourceIdentifiers.length);
+                    if (res.nextToken) {
+                        nextToken = res.nextToken;
+                        morepages = true;
+                    }
+                    else {
+                        morepages = false;
+                    }
+                }
             }
         }));
 
     }
 })).then(
-()=>{
-    console.info("Done. Total Resources " + total);
-}
+    () => {
+        console.info("Done. Total Resources " + total);
+    }
 );
